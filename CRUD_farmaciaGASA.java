9202d1c8fae16039b2acb2c8dbf26054410236dc
thalitaan <thalita.santos@aluno.ifsertao-pe.edu.br>
2025-02-24 10:02:42 -0300

    Add files via upload

---
+++ CRUD_farmaciaGASA.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ifsertaope.cs.crud_farmaciaGASA;
+
+import ifsertaope.cs.crud_farmaciaGASA.gui.FarmaciaLogin;
+
+/**
+ *
+ * @author talit
+ */
+public class CRUD_farmaciaGASA {
+    
+    public static void main (String []  args){
+        FarmaciaLogin objTL = new FarmaciaLogin ();
+        
+        objTL.setVisible (true);
+        
+        
+    }
+}
